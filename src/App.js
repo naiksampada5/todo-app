866366d03e9a823b0ca6c5279eb4d10a9309e5f2
Sampada Naik <naiksampada.5@gmail.com>
2020-06-20 15:52:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+// eslint-disable-next-line
+/* eslint-disable */
+import React, { Component } from 'react';
+import './App.css';
+import ListItems from './ListItems'
+//import { library } from './@fortawesome/fontawesome-svg-core';
+//import {faTrash} from '@fortawesome/free-solid-svg-icons';
+
+//library.add(faTrash);
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if(newItem.text!==""){
+      const newItems=[...this.state.items, newItem];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter Text"
+            value={this.state.currentItem.text}
+            onChange={this.handleInput} />
+            <button type="submit">Add</button>
+          
+          </form>
+        </header>
+        <ListItems items = {this.state.items}></ListItems>
+      </div>
+    );
+  }
+}
+
+export default App;
